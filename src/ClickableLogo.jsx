b7bdc7e1d89c5f3734edab29d581df7c963519bb
Tayfunmert <tayfunmert2001@googlemail.com>
2024-09-16 20:37:57 +0200

    Logo finished

---
+++ src/ClickableLogo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Logo2 from "../static/Logo2.png";
+
+const ClickableLogo = () => {
+  return (
+    <div>
+      <a href="https://youtu.be/dQw4w9WgXcQ?t=43" className="myLogo" target="_blank" rel="noopener noreferrer">
+      <img src={Logo2} alt="../static/Logo.png" className="logoImage" />
+      </a>
+    </div>
+  );
+};
+
+export default ClickableLogo;
